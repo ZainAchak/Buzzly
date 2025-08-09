0e552018f48949489673dcd1bf06f23bff6b104c
Xayn <zainullahachakzai@gmail.com>
2025-08-09 21:43:40 +0500

    added Profile

--- src/pages/Createpost.jsx
+++ src/pages/Createpost.jsx
@@ -1,3 +1,57 @@
+import { Image } from "lucide-react"
+import { dummyUserData } from "../assets/assets"
+import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom"
+
 export default function Createpost() {
-  return <h1>This is Createpost</h1>;
+  const user = dummyUserData
+  const navigate = useNavigate()
+
+   async function handleCreatePost() {
+    const wait = new Promise((resolve)=>setTimeout(() => {
+      resolve(null)
+    }, 2000))
+    await wait;
+    navigate("/")
+  }
+
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-6xl p-6 ">
+         {/* Title */}
+        <div className="m-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Create Post</h1>
+          <p className="text-slate-600">Share your thoughts with the world</p>
+        </div>
+      </div>
+
+      {/* Create Post */}
+
+      <div className="m-8 p-3  lg:w-180 bg-white shadow rounded-xl">
+          <div className="flex p-6 gap-2">
+            <img src={user.profile_picture} className="rounded-full h-16" alt="" />
+            <div className="mt-2">
+              <h1 className="font-bold text-slate-700 text-xl">{user.full_name}</h1>
+              <p className="text-slate-500">@{user.username}</p>
+            </div>
+          </div>
+          
+          <textarea rows={4} name="createpost" id="createpost" placeholder="whats happening" className="outline-none text-slate-600 text-md no-scrollbar p-4 w-full resize-none "/>
+
+          <div className="flex justify-between border-t-1 border-slate-200">
+            <Image className="mt-4 cursor-pointer text-slate-500" size={28} />
+            <button onClick={()=>toast.promise(
+              handleCreatePost(),{
+                loading: 'Posting...',
+                success: <p>Posted</p>,
+                error: e => <p>{e.message}</p>
+              }
+            )} className="mt-4 cursor-pointer bg-indigo-600 text-white px-12 py-2 rounded-md font-medium bg-gradient-to-r active:scale-110 hover:from-indigo-500 hover:to-indigo-700 transition-all duration-200">
+              Publish Post
+            </button>
+          </div>
+
+      </div>
+    </div>
+  )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { assets, dummyConnectionsData, dummyUserData } from "../assets/assets";
 import { BadgeCheck, Calendar, MapPin } from "lucide-react";
 import moment from "moment";
@@ -9,6 +9,7 @@ import ProfilePosts from "../components/ProfilePosts";
 
 export default function Profile() {
   const [selectedBtn, setSelectedBtn] = useState('posts')
+  const{data:posts, isPending, isSuccess} = useFeedsQuery()
   const params = useParams()
   const userId = params["profileId"]
   const options = ["posts", "media", "likes"];
@@ -66,12 +67,12 @@ export default function Profile() {
       </div>
 
       {/* Posts Media Likes */}
-      <div className=" flex flex-col flex-1 max-w-6xl m-3">
+      <div className=" rounded-xl flex flex-col flex-1 max-w-6xl m-3 mx-auto">
           {/* Buttons */}
-          <div className="w-full flex flex-wrap max-md:flex-col gap-0 sm:gap-2 justify-center items-center">
-              <button onClick={()=>setSelectedBtn("posts")} className={`${selectedBtn === "posts" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" } shadow cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center max-sm:rounded-t-2xl sm:rounded-md text-xl`}>Posts</button>
-              <button onClick={()=>setSelectedBtn("media")} className={`${selectedBtn === "media" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" } shadow cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center sm:rounded-md text-xl`}>Media</button>
-              <button onClick={()=>setSelectedBtn("likes")} className={`${selectedBtn === "likes" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" } shadow cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center max-sm:rounded-b-2xl sm:rounded-md text-xl`}>Likes</button>
+          <div className="overflow-clip rounded-xl max-w-200 flex  bg-slate-200 shadow max-md:flex-col gap-0 sm:gap-2 justify-center items-center ">
+              <button onClick={()=>setSelectedBtn("posts")} className={`${selectedBtn === "posts" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" }  cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center max-sm:rounded-t-2xl sm:rounded-md text-xl`}>Posts</button>
+              <button onClick={()=>setSelectedBtn("media")} className={`${selectedBtn === "media" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" }  cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center sm:rounded-md text-xl`}>Media</button>
+              <button onClick={()=>setSelectedBtn("likes")} className={`${selectedBtn === "likes" ? "bg-indigo-500 text-white" : "bg-slate-200 text-gray-700" }  cursor-pointer py-2 w-85 sm:w-45 md:w-45 flex items-center justify-center max-sm:rounded-b-2xl sm:rounded-md text-xl`}>Likes</button>
           </div>
             
         
@@ -82,6 +83,29 @@ export default function Profile() {
         selectedBtn === "posts" && 
         
           <ProfilePosts  />
+        
+      }
+
+      {/* Media */}
+      {
+        selectedBtn === "media" && 
+        
+          <div className="flex flex-wrap mb-2 justify-center gap-2 mt-6 max-x-6xl">
+            {
+              posts.filter((post)=> post.image_urls.length > 0).map((postWImg)=>(
+                <>
+                  {
+                    postWImg.image_urls.map((image,index)=>(
+                      <Link target="_blank" className="relative group" to={image}>
+                        <img src={image} className="w-74 aspect-video object-cover" alt="" />  
+                        <p className="absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl text-white opacity-0 group-hover:opacity-100 transition duration-300">Posted {moment(postWImg.createdAt).fromNow()}</p>                    
+                      </Link>
+                    ))
+                  }
+                </>
+              ))
+            }
+          </div>
         
       }
     </div>
